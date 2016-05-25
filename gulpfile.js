d2b905aca7284eee6ca006146f26206d1650857f
Jacob Hefley and Ming Wen <jacobhefley3@gmail.com>
2016-05-25 15:11:36 -0700

    npm, bower and sass

--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,9 @@ var lib = require('bower-files')({
 
 var browserSync = require('browser-sync').create();
 
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
     .pipe(concat('allConcat.js'))
@@ -57,6 +60,7 @@ gulp.task("build", ['clean'], function(){
     gulp.start('jsBrowserify');
   }
     gulp.start('bower');
+    gulp.start('cssBuild');
 });
 
 gulp.task('jshint', function(){
@@ -99,8 +103,17 @@ gulp.task('serve', function() {
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(["scss/*.scss"], ['cssBuild']);
 });
 
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
   browserSync.reload();
+});
+
+gulp.task('cssBuild', function() {
+  return gulp.src(['scss/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'));
 });
